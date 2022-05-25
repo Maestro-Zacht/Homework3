03eb5d4f122eb6ab356a528f4a13b1e7a8470ff7
Matteo Ghia <matteo.ghia@yahoo.it>
2022-05-26 00:34:52 +0200

    fixed db and first pages

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
 
 dialect = "mysql+pymysql"
 username = "root"
-psw = "dummyp@ssword45"
+psw = "dummypassword45"
 host = "db"
 dbname = "Cycling"
 
@@ -14,7 +15,21 @@ engine = create_engine(f"{dialect}://{username}:{psw}@{host}/{dbname}")
 
 @app.route('/')
 def index():
-    return 'Test'
+    return render_template('homepage.html')
+
+
+@app.route('/cyclist')
+def cyclist():
+    query = "SELECT CID, Name, Surname FROM Cyclist;"
+    try:
+        con = engine.connect()
+        res = con.execute(query).fetchall()
+        con.close()
+    except SQLAlchemyError as e:
+        print(f"Error! {e}")
+        raise e
+
+    return render_template('cyclist.html', cyclists=res)
 
 
 if __name__ == '__main__':
